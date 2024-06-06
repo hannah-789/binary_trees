919ae49457202837215ef89698e34dca33727422
hanae <hanaeelyaagoubi45@gmail.com>
2024-06-06 06:25:22 -0700

    rti

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,17 +8,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *xin_node;
+    binary_tree_t *new_node;
 
-    xin_node = malloc(sizeof(binary_tree_t));
+    new_node = malloc(sizeof(binary_tree_t));
     if (xin_node == NULL) {
         return NULL;  
     }
 
-    xin_node->n = value;
-    xin_node->parent = parent;
-    xin_node->left = NULL;
-    xin_node->right = NULL;
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-    return xin_node;
+    return new_node;
 }
